31085cdd184bb4f0272e8314c237010fc6652034
Mritunjay Gupta <66947124+Mritunjay004@users.noreply.github.com>
2021-07-18 17:27:25 +0530

    Add files via upload

---
+++ js/new-post.js
@@ -0,0 +1,27 @@
+/**
+ * TODO: Finish submitNewPost function to submit form data to the API
+ */
+
+const API_URL = 'http://localhost:3000/api/posts';
+
+const submitNewPost = () => {
+  // HINT: Use FormData to store data to send over
+  // HINT: Redirect the user to home page after successful submission
+  const title = document.getElementById('form-post-title').value;
+  const content = document.getElementById('form-post-content').value;
+  const input = document.getElementById('form-post-image');
+
+  let data = new FormData();
+  data.append('post-image', input.files[0]);
+  data.append('title', title);
+  data.append('content', content);
+
+  fetch(API_URL, {
+    method: 'POST',
+    body: data,
+  }).then(() => {
+    setTimeout(() => {
+      window.location.href = 'index.html';
+    }, 1000);
+  });
+};
---
+++ js/post.js
@@ -0,0 +1,46 @@
+/**
+ * TODO (Together): Create getPostIdParam to get the id of the post to use in the request later
+ * TODO: Complete getPost function to get post data from API
+ * TODO: Complete buildPost function to fill in the post data in the post.html file using ids
+ */
+
+const API_URL = 'http://localhost:3000/api/posts/';
+const API_BASE_URL = 'http://localhost:3000/';
+
+window.onload = () => {
+  getPost();
+};
+
+const getPostIdParam = () => {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  return urlParams.get('id');
+};
+
+// /api/posts/:post_id
+const getPost = () => {
+  const postId = getPostIdParam();
+  const route = `${API_URL}${postId}`;
+  // CODE GOES HERE
+  fetch(route, {
+    method: 'Get',
+  })
+    .then(response => {
+      return response.json();
+    })
+    .then(data => {
+      buildPost(data);
+    });
+};
+
+const buildPost = data => {
+  // HINT: Convert the date number to a Date string
+  const postImage = `${API_BASE_URL}${data.post_image}`;
+  document.querySelector('header').style.backgroundImage = `url(${postImage})`;
+  document.getElementById('individual-post-title').innerText = data.title;
+  const date = new Date(parseInt(data.added_date, 10)).toDateString();
+  document.getElementById(
+    'individual-post-date'
+  ).innerText = `Published on ${date}`;
+  document.getElementById('individual-post-content').innerText = data.content;
+};
